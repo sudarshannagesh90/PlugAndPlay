1b4ff954a48dd8e32041384c11745c15749677bd
sudarshan <sudarshannagesh90@gmail.com>
2018-04-03 10:20:22 -0500

    Adding specific noise level

--- computeCostFunction.m
+++
@@ -1,4 +0,0 @@
-function  costFunction = computeCostFunction(c,mu,r,sigmaLambda,rtilde)
-
-epsilon = 1e-5;
-costFunction = sum(log10(r(:)+epsilon))+sum(sum((1./r(:)).*(c(:)+abs(mu(:)).^2)))+(1/(2*sigmaLambda^2))*norm(r-rtilde)^2;
--- computeCovarianceAndMean.m
+++
@@ -1,5 +0,0 @@
-function [c,mu] = computeCovarianceAndMean(y,sigma_w,r)
-%% Compute c and mu 
-c  = (1/sigma_w^2+1./r(:)).^(-1);                                            % diagonal-elements of cov. matrix 
-inv= ifft2(y);                                                               % fourier-inverse of the observation
-mu = (1/sigma_w^2)*c.*inv(:);                                                % mean of the posterior-distribution
